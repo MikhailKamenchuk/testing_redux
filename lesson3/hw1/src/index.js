bb6cfb2a53de1f20b6752b4545de3ff7f0d916fb
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-09-08 17:32:12 +0300

    added lesson3 hw1

---
+++ lesson3/hw1/src/index.js
@@ -0,0 +1,6 @@
+import store from './store';
+import {setUser, removeUser} from './user.actions';
+import {setLanguage} from './language.actions';
+import {addProduct, removeProduct} from './cart.actions';
+
+
---
+++ lesson3/hw1/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, combineReducers } from "redux";
+import { userReducer } from './user.reducer';
+import { languageReducer } from './language.reducer';
+import { cartReducer } from './cart.reducer';
+
+
+const reducer = combineReducers({
+  cart: cartReducer,
+  language: languageReducer,
+  user: userReducer
+})
+
+const store = createStore(reducer);
+
+export default store
+
