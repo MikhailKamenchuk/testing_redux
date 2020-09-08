dcece66b7e064b9996c3a71cf9b40fbbdd37b64d
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-09-08 12:41:07 +0300

    added lesson2 task1

---
+++ lesson2/task1/src/index.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+export const increment = () => {
+  return {
+    type: INCREMENT
+  }
+}
+
+export const decrement = () => {
+  return {
+    type: DECREMENT
+  }
+}
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return store;
+  }
+}
+
+export const store = createStore(counterReducer);
+
+
+
+
