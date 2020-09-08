0e9689916daca1e5991339b1dc85cdef41010d45
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-09-08 14:27:37 +0300

    added lesson2 hw1

---
+++ lesson2/hw1/src/index.js
@@ -0,0 +1,11 @@
+import store from './store';
+import {addUser, deleteUser} from './users.actions';
+
+// store.dispatch(addUser({ id: 76, name: 'Sarah' }));
+// store.dispatch(addUser({ id: 77, name: 'фцв' }));
+// store.dispatch(addUser({ id: 78, name: 'йцв' }));
+// store.dispatch(addUser({ id: 79, name: 'цйв' }));
+// console.log(store.getState())
+
+// store.dispatch(deleteUser(78));
+// console.log(store.getState())
---
+++ lesson2/hw1/src/store.js
@@ -0,0 +1,7 @@
+import { createStore } from "redux";
+import { reducer } from './users.reducer';
+
+const store = createStore(reducer);
+
+export default store
+
