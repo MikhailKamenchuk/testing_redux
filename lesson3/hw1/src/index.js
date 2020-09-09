8e7b6db5ce78a16587fa3c2bdcf51ba7acb20f2e
Mikhail Kamenchuk <kamenchuk.m@gmail.com>
2020-09-09 12:33:08 +0300

    added lesson4 task1

--- lesson3/hw1/src/index.js
+++
@@ -1,6 +0,0 @@
-import store from './store';
-import {setUser, removeUser} from './user.actions';
-import {setLanguage} from './language.actions';
-import {addProduct, removeProduct} from './cart.actions';
-
-
